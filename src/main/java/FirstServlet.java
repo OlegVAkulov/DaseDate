ba3b179a8d0cb7212b4e28babb5a35493a143c70
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-12 12:04:37 +0300

    1

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -8,17 +8,27 @@ import java.io.PrintWriter;
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null){
+            session.setAttribute("count", 1);
+        }else {
+            session.setAttribute("count", count + 1);
+        }
+
+
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
 
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
-        pw.println("<h1>Privet, " + name + " " + surname + "</h1>");
+        pw.println("<h1>Your count is: " + count + "</h1>");
+//        pw.println("<h1>Privet, " + name + " " + surname + "</h1>");
         pw.println("</html>");
 
 //        response.sendRedirect("/index.jsp");
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
-        dispatcher.forward(request, response);
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+//        dispatcher.forward(request, response);
     }
 
     @Override
