3a3c0c6799ce4acde2c331f7efc0011230b885e6
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-12 10:25:43 +0300

    1

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -8,9 +8,11 @@ import java.io.PrintWriter;
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
-        pw.println("<h1>Vsem Privet!</h1>");
+        pw.println("<h1>Privet, " + name + "</h1>");
         pw.println("</html>");
     }
 
