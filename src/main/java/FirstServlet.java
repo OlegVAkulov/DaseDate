c4450e3070ca0798e0b9cbd3bce0cf70324c1c98
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-12 18:41:26 +0300

    add

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -23,7 +23,7 @@ public class FirstServlet extends HttpServlet {
                     "jdbc:postgresql://192.168.0.32:5432/test_db",
                     "postgres", "123qwerty321");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT name from people");
+            ResultSet resultSet = statement.executeQuery("SELECT title from people");
             while (resultSet.next()){
                 pw.println(resultSet.getString("name"));
             }
