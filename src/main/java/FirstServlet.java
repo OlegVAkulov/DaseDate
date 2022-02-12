74e174764a72db99d65568b3b1b05e1743a23d0c
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-12 14:56:48 +0300

    1

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -4,12 +4,33 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(name = "FirstServlet", value = "/FirstServlet")
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
 
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://192.168.0.32:5432/test_db",
+                    "postgres", "123qwerty321");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT title from people");
+            while (resultSet.next()){
+                pw.println(resultSet.getString("name"));
+            }
+            statement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
