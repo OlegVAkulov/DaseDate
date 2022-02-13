38d88d18e33dfab3fc13b2501835e4bee39d984e
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 07:15:52 +0300

    add

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -25,8 +25,8 @@ public class FirstServlet extends HttpServlet {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * from people");
             while (resultSet.next()){
-                pw.println(resultSet.getString("name"));
-                pw.println(resultSet.getString("surname"));
+                pw.print(resultSet.getString("name" + "  "));
+                pw.print(resultSet.getString("surname" + "  "));
                 pw.println(resultSet.getString("age"));
 
 
