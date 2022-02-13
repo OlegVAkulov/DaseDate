c43d08f9c500c7a9abc61903db23b8e9c9a20625
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 07:56:45 +0300

    add

--- src/main/java/GetCookiesServlet.java
+++ src/main/java/GetCookiesServlet.java
@@ -4,12 +4,14 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.Scanner;
 
 @WebServlet(name = "GetCookiesServlet", value = "/GetCookiesServlet")
 public class GetCookiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter pw = response.getWriter();
+        Scanner scanner = new Scanner(System.in);
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -22,6 +24,13 @@ public class GetCookiesServlet extends HttpServlet {
                     "postgres", "123qwerty321");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * from people");
+            pw.println("INPUT NAME:");
+            String title = scanner.nextLine();
+            pw.println("INPUT AGE:");
+            int pages = scanner.nextInt();
+            pw.println(title + "\t\t" + pages);
+
+
             pw.println("NAME\t\tSURNAME\t\tAGE");
             pw.println();
             while (resultSet.next()){
