0299db540addb70007a557c746b149859f704872
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 09:02:03 +0300

    add

--- src/main/java/GetCookiesServlet.java
+++ src/main/java/GetCookiesServlet.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class GetCookiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("/index.jsp");
+        response.sendRedirect("https://www.google.com");
 
         PrintWriter pw = response.getWriter();
         Scanner scanner = new Scanner(System.in);
