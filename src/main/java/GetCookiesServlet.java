124e6fd3dac7bf282764af04d99c970e025e0246
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 09:06:36 +0300

    add

--- src/main/java/GetCookiesServlet.java
+++ src/main/java/GetCookiesServlet.java
@@ -10,38 +10,38 @@ import java.util.Scanner;
 public class GetCookiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter pw = response.getWriter();
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://192.168.0.32:5432/test_db",
-                    "postgres", "123qwerty321");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * from people");
-            pw.println("ID\tNAME\t\tSURNAME\t\tAGE");
-            pw.println();
-            while (resultSet.next()){
-                pw.print(resultSet.getString("id"));
-                pw.print("\t");
-                pw.print(resultSet.getString("name"));
-                pw.print("\t\t");
-                pw.print(resultSet.getString("surname" ));
-                pw.print("\t\t");
-                pw.println(resultSet.getString("age"));
-
-
-
-            }
-            statement.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-//        response.sendRedirect("https://www.google.com");
+//        PrintWriter pw = response.getWriter();
+//
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            Connection connection = DriverManager.getConnection(
+//                    "jdbc:postgresql://192.168.0.32:5432/test_db",
+//                    "postgres", "123qwerty321");
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * from people");
+//            pw.println("ID\tNAME\t\tSURNAME\t\tAGE");
+//            pw.println();
+//            while (resultSet.next()){
+//                pw.print(resultSet.getString("id"));
+//                pw.print("\t");
+//                pw.print(resultSet.getString("name"));
+//                pw.print("\t\t");
+//                pw.print(resultSet.getString("surname" ));
+//                pw.print("\t\t");
+//                pw.println(resultSet.getString("age"));
+//
+//
+//
+//            }
+//            statement.close();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+        response.sendRedirect("https://www.google.com");
 
     }
 
