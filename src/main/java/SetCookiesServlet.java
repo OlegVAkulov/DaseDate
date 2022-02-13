849142516e3a37c2d2c37b091115a737eb1963b0
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 18:48:25 +0300

    add

--- src/main/java/SetCookiesServlet.java
+++ src/main/java/SetCookiesServlet.java
@@ -1,3 +1,6 @@
+import logic.User;
+import logic.UserDao;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -8,81 +11,29 @@ import java.sql.*;
 
 @WebServlet(name = "SetCookiesServlet", value = "/SetCookiesServlet")
 public class SetCookiesServlet extends HttpServlet {
-    String name1;
-    String surname1;
-    int age1;
+    private static final long serialVersionUID = 1L;
+    private UserDao userDAO= new UserDao();
+
+    public void init() {
+        userDAO = new UserDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-        PrintWriter pw = response.getWriter();
-
-
-        pw.println("<head>");
-        pw.println("<body>");
-        pw.println("<h3>Input new element</h3>");
-        pw.println("<form>");
-        pw.println("<strong>User Name</strong>:<input type=\"text\" name=\"name\"><br>");
-        pw.println("<strong>User Surname</strong>:<input type=\"text\" name=\"surname\"><br>");
-        pw.println("<strong>User Age</strong>:<input type=\"number\" name=\"age\"><br>");
-        pw.println("<input type=\"submit\" value=\"INSERT\">");
-        pw.println("</form>");
-        pw.println("</body>");
-        pw.println("</head>");
+        int id = Integer.parseInt(request.getParameter("id"));
         String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        String age = request.getParameter("age");
+        String surname = request.getParameter("email");
+        int age = Integer.parseInt(request.getParameter("age"));
 
-        Cookie cookie1 = new Cookie("name", name);
-        Cookie cookie2 = new Cookie("surname", surname);
-        Cookie cookie3 = new Cookie("age", age);
-        cookie1.setMaxAge(24 * 60 * 60);
-        cookie2.setMaxAge(24 * 60 * 60);
-        cookie3.setMaxAge(24 * 60 * 60);
-
-        name1 = String.valueOf(cookie1);
-        surname1 = String.valueOf(cookie2);
-        age1 = Integer.parseInt(String.valueOf(cookie3));
-
+        User book = new User(name, surname, age);
         try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://192.168.0.32:5432/test_db",
-                    "postgres", "123qwerty321");
-
-
-            PreparedStatement preparedStatement = connection.prepareStatement
-                    ("INSERT into people(name, surname,age) VALUES (name1, surname1, age1)");
-
-            preparedStatement.executeUpdate();
-            connection.close();
+            userDAO.updateUser(book);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        response.sendRedirect("list");
 
-
-        response.addCookie(cookie1);
-        response.addCookie(cookie2);
-        response.addCookie(cookie3);
-
-
-        pw.println("<head>");
-        pw.println("<a href=\"FirstServlet\">Data Base</a>");
-        pw.println("</head>");
-
-
-
-//        Cookie[] cookies = request.getCookies();
-//        pw.println("<head>");
-//        for (Cookie cookie : cookies) {
-//            pw.println("<h1>" + cookie.getName() + "  " + cookie.getValue() + "</h1>");
-//        }
-//        pw.println("</head>");
-//        pw.println("<a href=\"GetCookiesServlet\">Add to Base</a>");
     }
 
     @Override
