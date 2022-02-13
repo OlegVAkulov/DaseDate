65eed0dc55843271cd2e55ce3297e30b20e6de4e
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-13 12:30:43 +0300

    add

--- src/main/java/GetCookiesServlet.java
+++ src/main/java/GetCookiesServlet.java
@@ -12,22 +12,54 @@ import java.util.Scanner;
 public class GetCookiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Cookie[] cookies = request.getCookies();
+//        Cookie[] cookies = request.getCookies();
+//        Cookie name = cookies[0];
+//        Cookie surname = cookies[1];
+//        Cookie age = cookies[2];
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        String surname = (String) session.getAttribute("surname");
+        Integer age = (Integer) session.getAttribute("age");
+
         PrintWriter pw = response.getWriter();
-        pw.println("<head>");
-        for (Cookie cookie : cookies) {
-            pw.println("<h1>" + cookie.getName() + "  " + cookie.getValue() + "</h1>");
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        pw.println("</head>");
-
-
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://192.168.0.32:5432/test_db",
+                    "postgres", "123qwerty321");
+            Statement statement = connection.createStatement();
+            statement.executeQuery("INSERT into people(name, surname,age) VALUES (name, surname, age)");
+            ResultSet resultSet = statement.executeQuery("SELECT * from people");
+            pw.println("ID\tNAME\t\tSURNAME\t\tAGE");
+            pw.println();
+            while (resultSet.next()) {
+                pw.print(resultSet.getString("id"));
+                pw.print("\t");
+                pw.print(resultSet.getString("name"));
+                pw.print("\t\t");
+                pw.print(resultSet.getString("surname"));
+                pw.print("\t\t");
+                pw.println(resultSet.getString("age"));
+            }
+            statement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+//        pw.println("<head>");
+//        for (Cookie cookie : cookies) {
+//            pw.println("<h1>" + cookie.getName() + "  " + cookie.getValue() + "</h1>");
+//        }
+//        pw.println("</head>");
 //        HttpSession session = request.getSession();
 //        User user = (User)session.getAttribute("user");
 //
 //
 //        RequestDispatcher dispatcher =  request.getRequestDispatcher("/showCart.jsp");
 //        dispatcher.forward(request,response);
-
     }
 
     @Override
